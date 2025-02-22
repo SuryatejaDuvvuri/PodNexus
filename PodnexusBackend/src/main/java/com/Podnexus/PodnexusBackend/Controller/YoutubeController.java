d47ba4111838b42a0838b3fe822c599ce3a23617
Suryateja Duvvuri <duvvurisuryateja95@gmail.com>
2025-02-22 14:34:59 -0800

    Add cross origin Bean

--- PodnexusBackend/src/main/java/com/Podnexus/PodnexusBackend/Controller/YoutubeController.java
+++ PodnexusBackend/src/main/java/com/Podnexus/PodnexusBackend/Controller/YoutubeController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import com.Podnexus.PodnexusBackend.Service.YoutubeService;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:3000")
 public class YoutubeController 
 {
     @Autowired
