8309edc1ccfd3c96df567025f74141c06a83fbc1
Suryateja Duvvuri <duvvurisuryateja95@gmail.com>
2025-02-22 13:48:03 -0800

    Fix dependencies and use youtubedl to process the audio

--- PodnexusBackend/src/main/java/com/Podnexus/PodnexusBackend/Controller/YoutubeController.java
+++ PodnexusBackend/src/main/java/com/Podnexus/PodnexusBackend/Controller/YoutubeController.java
@@ -1,7 +1,12 @@
 package com.Podnexus.PodnexusBackend.Controller;
 
+import java.util.Map;
+
 import org.apache.catalina.connector.Response;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.Podnexus.PodnexusBackend.Service.YoutubeService;
@@ -10,6 +15,7 @@ import com.Podnexus.PodnexusBackend.Service.YoutubeService;
 @RequestMapping("/api")
 public class YoutubeController 
 {
+    @Autowired
     private final YoutubeService youtubeService;
 
     public YoutubeController(YoutubeService youtubeService)
@@ -17,23 +23,28 @@ public class YoutubeController
         this.youtubeService = youtubeService;
     }
 
-    @RequestMapping("/audio")
-    public String getAudio()
+    @PostMapping("/processAudio")
+    public ResponseEntity<String> processLink(@RequestBody Map<String, String> request)
     {
-        return youtubeService.getAudio();
-    }
+        String link = request.get("youtubeUrl");
+        if(link == null || link.isEmpty())
+        {
+            return ResponseEntity.badRequest().body("Link is not valid");
+        }
+        else
+        {
+            try
+            {
+                String output = youtubeService.extractAudio(link);
+                return ResponseEntity.ok().body("Link is recieved: " + output);
+            }
+            catch(Exception e)
+            {
+                // System.out.println(e.getMessage());
+                return ResponseEntity.badRequest().body(e.getMessage());
+            }
+        }
 
-    @PostMapping("/audio")
-    public ResponseEntity<String> postAudio(@RequestBody YoutubeService request)
-    {
-        try
-        {
-            youtubeService.extractAudio(request.getUrl());
-            return ResponseEntity.ok("Audio extracted");
-        }
-        catch (Exception e)
-        {
-            return ResponseEntity.badRequest();
-        }
+       
     }
 }
--- PodnexusBackend/src/main/java/com/Podnexus/PodnexusBackend/Service/YoutubeService.java
+++ PodnexusBackend/src/main/java/com/Podnexus/PodnexusBackend/Service/YoutubeService.java
@@ -1,32 +1,34 @@
 package com.Podnexus.PodnexusBackend.Service;
 import org.springframework.stereotype.Service;
+
+import com.google.api.client.util.Value;
 import com.sapher.youtubedl.YoutubeDL;
 import com.sapher.youtubedl.YoutubeDLRequest;
 import com.sapher.youtubedl.YoutubeDLResponse;
 @Service
 public class YoutubeService {
     
-    private String url;
-    
-    public String getUrl()
-    {
-        return url;
-    }
+    // @Value({"${youtube.dl.path}"})
+    // private String youtubeDLPath;
 
-    public void setUrl()
+    public String extractAudio(String url) throws Exception
     {
-        this.url = url;
-    }
+        try
+        {
+            YoutubeDL.setExecutablePath("yt-dlp");
+            YoutubeDLRequest request = new YoutubeDLRequest(url);
+            request.setOption("extract-audio");
+            request.setOption("audio-format", "mp3");
+            request.setOption("output", "%(title)s.%(ext)s");
+            YoutubeDLResponse response = YoutubeDL.execute(request);
+            return response.getOut();
+        }
+        catch(Exception e)
+        {
+            System.err.println(e.getMessage());
+            return "Audio couldn't be processed.";
+        }
 
-    public String extractAudio(String url)
-    {
-        YoutubeDLRequest request = new YoutubeDLRequest(url);
-        request.setOption("extract-audio");
-        request.setOption("audio-format", "mp3");
-        request.setOption("output", "%(title)s.%(ext)s");
-
-        YoutubeDLResponse response = YoutubeDL.execute(request);
-        return response.getOut();
 
     }
 }
